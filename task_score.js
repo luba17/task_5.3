709942a5cac86b1f9d869c3f2a494badba8e28be
luba17 <lm@crosslife.me>
2023-07-01 02:48:15 +0300

    5.3 added task_score.js

---
+++ task_score.js
@@ -0,0 +1,10 @@
+let vvedenie = 10;
+let git = 50;
+let js = 60;
+
+let averagePoint = (vvedenie + git + js)/3;
+
+let nameStudent = "Luba";
+let surnameStudent = "Muratova";
+
+console.log(`${nameStudent} ${surnameStudent}: average point = ${averagePoint} `);
