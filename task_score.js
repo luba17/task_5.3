e51f828d1bacc39a3f73c0fa714dc19c0a046b2f
luba17 <lm@crosslife.me>
2023-07-03 11:58:39 +0300

    task_5.4 (used method slice)

--- task_score.js
+++ task_score.js
@@ -1,10 +1,10 @@
-let vvedenie = 10;
+let intro = 10;
 let git = 50;
 let js = 60;
 
-let averagePoint = (vvedenie + git + js)/3;
+let averagePoint = (intro + git + js) / 3;
 
 let nameStudent = "Luba";
 let surnameStudent = "Muratova";
 
-console.log(`${nameStudent} ${surnameStudent}: average point = ${averagePoint} `);
+console.log(`${nameStudent.slice(0,-3)}. ${surnameStudent}: average point = ${averagePoint}`);
